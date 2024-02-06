2b909f414b8ebb3f809289bad5e802a1cae21cd8
randall2002 <hbxie@phy.ecnu.edu.cn>
2024-02-06 22:50:29 +0800

    train: 整理文件：结构化。减小函数长度，减少循环层次。

--- train.py
+++ train.py
@@ -35,48 +35,20 @@ def train_model(model, criterion, optimizer, traindataloader, valdataloader, num
         print('-' * 40)
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
 
-        train_loss = 0.0
-        train_num = 0
-        val_loss = 0.0
-        val_num = 0
-        model.train()     # train modality
-        for step, batch_data in enumerate(traindataloader):
-            in_noisy_images, (gt_maps, gt_noiseless_images, tissue_image), _ = batch_data
-            optimizer.zero_grad()
-            in_noisy_images = in_noisy_images.to(device)
-            gt_maps = gt_maps.to(device)
-            gt_noiseless_images =gt_noiseless_images.to(device)
-
-
-            out = model(in_noisy_images)
-            loss = criterion(out, gt_maps)#可能网络需要输出s0,并把S0跟无噪图相比完善loss.
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item() * len(gt_maps)
-            train_num += len(gt_maps)
-        # 计算一个epoch在训练集上的精度和损失
-        train_loss_all.append(train_loss / train_num)
-        print('{} Train Loss:{:.4f}'.format(epoch, train_loss_all[-1]))
+        train_loss_sample = do_train_for_every_epoch(model, criterion, optimizer, traindataloader)
+        # 把每一次epoch在训练集上的样本平均损失 添加到列表里
+        train_loss_all.append( train_loss_sample )
+        print('{} Train Loss:{:.4f}'.format(epoch, train_loss_sample))
 
         time_use1 = time.time() - since
         print("train complete in {:.0f}m {:.0f}s".format(time_use1 // 60, time_use1 % 60))
         since2 = time.time()
 
-        # 计算一个epoch训练后在验证集上的损失
-        model.eval()
+        val_loss_sample = do_evla_for_every_epoch(model, criterion, optimizer, valdataloader)
 
-        for step, batch_data in enumerate(valdataloader):
-            in_noisy_images, (gt_maps, gt_noiseless_images, tissue_image), _ = batch_data
-            in_noisy_images = in_noisy_images.to(device)
-            gt_maps = gt_maps.to(device)
-            gt_noiseless_images = gt_noiseless_images.float().to(device)
-            out = model(in_noisy_images)    # 傅里叶变换后的图像作为输入
-            loss = criterion(out, gt_maps)
-            val_loss += loss.item() * len(gt_maps)
-            val_num += len(gt_maps)
         # 计算一个epoch在验证集上的精度和损失
-        val_loss_all.append(val_loss / val_num)
-        print('{} Val Loss:{:.4f}'.format(epoch, val_loss_all[-1]))
+        val_loss_all.append(val_loss_sample)
+        print('{} Val Loss:{:.4f}'.format(epoch, val_loss_sample))
 
         # 保存最好的网络参数
         if val_loss_all[-1] < best_loss:
@@ -97,6 +69,44 @@ def train_model(model, criterion, optimizer, traindataloader, valdataloader, num
     model.load_state_dict(best_model_wts)
     return model, train_process
 
+
+def do_train_for_every_epoch(model, criterion, optimizer, traindataloader):
+    train_loss = 0.0#训练集上的总损失；
+    train_count = 0#训练集总样本数, num会有歧义（能表示序号和总数）
+    model.train()  # train modality
+    for step, batch_data in enumerate(traindataloader):
+        in_noisy_images, (gt_maps, gt_noiseless_images, tissue_image), _ = batch_data
+        optimizer.zero_grad()
+        in_noisy_images = in_noisy_images.to(device)
+        gt_maps = gt_maps.to(device)
+        gt_noiseless_images = gt_noiseless_images.to(device)
+
+        out = model(in_noisy_images)
+        loss = criterion(out, gt_maps)  # 可能网络需要输出s0,并把S0跟无噪图相比完善loss.
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item() * len(gt_maps)
+        train_count += len(gt_maps)
+
+    return train_loss/train_count #返回平均损失
+
+def do_evla_for_every_epoch(model, criterion, optimizer, valdataloader):
+    val_loss = 0.0#验证集上的总损失
+    val_count = 0 #验证集样本数
+    model.eval()
+    for step, batch_data in enumerate(valdataloader):
+        in_noisy_images, (gt_maps, gt_noiseless_images, tissue_image), _ = batch_data
+        in_noisy_images = in_noisy_images.to(device)
+        gt_maps = gt_maps.to(device)
+        gt_noiseless_images = gt_noiseless_images.float().to(device)
+        out = model(in_noisy_images)  # 傅里叶变换后的图像作为输入
+        loss = criterion(out, gt_maps)
+        val_loss += loss.item() * len(gt_maps)
+        val_count += len(gt_maps)
+
+    return val_loss/val_count
+
+##################################################
 def main():
 
     global opt, model
@@ -111,8 +121,6 @@ def main():
     use_cuda = True if torch.cuda.is_available() else False
     if use_cuda:
         torch.cuda.manual_seed(seed)
-
-
     #---------------------------
 
     train_dir = opt.traindir
@@ -136,7 +144,7 @@ def main():
     unet = U_Net(in_ch=8, out_ch=3).to(device) #1,设法读取数据后实例化模型；2，需要考虑s0是否送入网络。
     #定义损失函数和优化器
     LR = 0.003
-    criterion = nn.MSELoss(reduction='sum')
+    criterion = nn.MSELoss(reduction='mean')
     optimizer = optim.Adam(unet.parameters(), lr=LR,  weight_decay=0)
     # 对模型迭代训练，所有数据训练epoch轮
     net, train_process = train_model(unet, criterion, optimizer, train_dataloader, valid_dataloader, num_epochs=25)
