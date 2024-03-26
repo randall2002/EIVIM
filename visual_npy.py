184a2bef6e4a9052a14dfd751ed775ceba7ab6be
pioneer23333 <jlwkmust@163.com>
2024-03-26 14:04:15 +0800

    增加目前的测试代码以及测试的dataset；增加训练过程的tensorboard

--- visual_npy.py
+++ visual_npy.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-depthmap = np.load('/homes/lwjiang/Data/IVIM/Result/0001.npy')    #使用numpy载入npy文件
-for i in range(depthmap.shape[2]):
+depthmap = np.load('/homes/lwjiang/Data/IVIM/public_training_data/training1/0001_IVIMParam.npy')    #使用numpy载入npy文件
+print("深度图的形状:", depthmap.shape)  
+
+for i in range(3):
     plt.imshow(depthmap[:, :, i])              #执行这一行后并不会立即看到图像，这一行更像是将depthmap载入到plt里
     plt.savefig(f'depthmap{i+1}.jpg')       #执行后可以将文件保存为jpg格式图像，可以双击直接查看。也可以不执行这一行，直接执行下一行命令进行可视化。但是如果要使用命令行将其保存，则需要将这行代码置于下一行代码之前，不然保存的图像是空白的
     plt.show()                        #在线显示图像
